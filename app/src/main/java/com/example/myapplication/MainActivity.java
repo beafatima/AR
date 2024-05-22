d1aae793131b07eab36b9d87c4bb73181cc4a778
Bea Fatima Dingson <dingson.176198@dasmarinas.sti.edu.ph>
2024-05-22 10:13:02 +0800

    changes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -17,19 +18,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    //RecyclerView Adapter
-    private MyItemAdapter adapter;
-    private ImageView imageView;
-    private ImageView imageView2;
+    private ImageView imageView, imageView2;
     private FrameLayout popUpContainer;
     private ArrayList<String> favoriteItems;
-
-    //tops & bottoms
-    private TextView tops;
-    private TextView bottoms;
-    private FrameLayout popUpTops;
-    private FrameLayout popUpBottoms;
 
 
     @SuppressLint("MissingInflatedId")
@@ -39,68 +30,14 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        //BottomNavigation
-   /*     BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
-        bottomNav.setSelectedItemId(R.id.home);
 
-        bottomNav.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
-                case R.id.home:
-                    return true;
-                case R.id.android:
-                    startActivity(new Intent(getApplicationContext(), Categories.class));
-                    finish();
-                    return true;
-                case R.id.add:
-                    startActivity(new Intent(getApplicationContext(), Settings.class));
-                    finish();
-                    return true;
-            }
-            return false;
-        });
-*/
         imageView = findViewById(R.id.heart);
-        imageView2 = findViewById(R.id.heart2);
+        imageView2 = findViewById(R.id.memaheart2);
         popUpContainer = findViewById(R.id.pop_up_container);
         favoriteItems = new ArrayList<>();
 
 
 
-        //tops & bottoms
-        popUpTops = findViewById(R.id.pop_up_tops);
-        popUpBottoms = findViewById(R.id.pop_up_bottoms);
-        tops = findViewById(R.id.tops);
-        bottoms = findViewById(R.id.bottoms);
-
-        tops.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //openTopsSection();
-                openCategories();
-                //showPopUpTops();
-            }
-        });
-        //TRY ---------------------------------------------------------------------------------
-        bottoms.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //openTopsSection();
-                openCategories();
-
-            }
-        });
-
-
-        //RecyclerView Adapter - not yet finished
-// RecyclerView recyclerView = findViewById(R.id.recycler_view);
-// recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//
-// List<MyDataItem> data = new ArrayList<>();
-// data.add(new MyDataItem("Item 1"));
-// data.add(new MyDataItem("Item 2"));
-//
-// adapter = new MyItemAdapter(data);
-// recyclerView.setAdapter(adapter);
         //clicked heart
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,16 +60,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-    //Categories -- when tops and bottoms clicked
-
-    private void showPopUpTops(){
-        View popUpView = getLayoutInflater().inflate(R.layout.pop_up_layout_tops, null);
-
-        popUpTops.removeAllViews();
-        popUpTops.addView(popUpView);
-
-        popUpTops.setVisibility(View.VISIBLE);
     }
     private void hidePopUpCategories(){
         popUpContainer.setVisibility(View.GONE);
@@ -165,10 +92,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //once item is clicked - filled heart
-    private void toggleFavorite(){
+    private void toggleFavorite() {
         boolean isFavorited = favoriteItems.contains("item");
 
-        if(isFavorited){
+        if (isFavorited) {
             favoriteItems.remove("item");
             imageView2.setImageResource(R.drawable.heart_no_color);
         } else {
@@ -176,13 +103,6 @@ public class MainActivity extends AppCompatActivity {
             imageView2.setImageResource(R.drawable.filled_heart);
         }
     }
-    private void updateFavoritesPage(){
-        //RecyclerView recyclerView =
-    }
-    //tops & bottoms
-    private void openCategories(){
-        Intent intent = new Intent(this, Categories.class);
-        startActivity(intent);
-    }
+
 
 }
--- app/src/main/java/com/example/myapplication/Settings.java
+++ app/src/main/java/com/example/myapplication/Settings.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Settings extends AppCompatActivity {
 
@@ -20,5 +23,26 @@ public class Settings extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+  /*      BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setSelectedItemId(R.id.add);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                    return true;
+                case R.id.android:
+                    startActivity(new Intent(getApplicationContext(), Categories.class));
+                    finish();
+                    return true;
+                case R.id.add:
+                    startActivity(new Intent(getApplicationContext(), Settings.class));
+                    finish();
+                    return true;
+            }
+            return false;
+        });
+*/
     }
 }
